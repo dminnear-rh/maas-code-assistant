4204fbfe7eae07a30d7853a3feb442bc46ffa28e
James Harmison <jharmison@redhat.com>
2026-03-06 10:47:06 -0500

    fix: fall back to default router-certs-default secret
    fixes: #5

--- charts/maas-code-assistant/render.sh
+++ charts/maas-code-assistant/render.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-INGRESS_CERTIFICATE=$(oc get ingresscontroller -n openshift-ingress-operator default -ojsonpath='{.spec.defaultCertificate.name}' 2>/dev/null)
 INGRESS_DOMAIN=$(oc get ingresscontroller -n openshift-ingress-operator default -ojsonpath='{.status.domain}' 2>/dev/null)
-export INGRESS_CERTIFICATE INGRESS_DOMAIN
-
-if [ -z "$INGRESS_CERTIFICATE" ] || [ -z "$INGRESS_DOMAIN" ]; then
+if [ -z "$INGRESS_DOMAIN" ]; then
   echo "Unable to retrieve ingress configuration from your cluster." >&2
-  echo "Are you logged in with oc, and do you have a valid wildcard certificate configured for the router?" >&2
+  echo "Are you logged in with oc?" >&2
+  oc whoami
   exit 1
 fi
+export INGRESS_DOMAIN
+
+INGRESS_CERTIFICATE=$(oc get ingresscontroller -n openshift-ingress-operator default -ojsonpath='{.spec.defaultCertificate.name}' 2>/dev/null)
+if [ -z "$INGRESS_CERTIFICATE" ]; then
+  INGRESS_CERTIFICATE=router-certs-default
+fi
+export INGRESS_CERTIFICATE
 
 cd "$(dirname "$(realpath "$0")")"
 
